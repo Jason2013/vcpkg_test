3c5f332bbdae50cd3563b4ece3641eaa12b52a68
Jason Chen <shchenchang@gmail.com>
2018-11-04 18:09:13 +0800

    install packages by platform

--- build.py
+++ build.py
@@ -1,41 +1,71 @@
 # coding=utf-8
 
 import os
+import sys
 
-print(os.environ["APPVEYOR_BUILD_WORKER_IMAGE"])
-print(os.environ["Configuration"])
-print(os.environ["Platform"])
+def InstallScript():
+    packages = [
+        "glew", 
+        "glfw3",
+        "glm",
+    ]
+    pass
+    # vcpkg install --triplet=x86-windows:x64-windows <pkg>
+    Platform = os.environ["Platform"]
+    assert(Platform in ["x86", "x64"])
+    ERROR_COMMAND = 'IF %ERRORLEVEL% NEQ 0 EXIT /B 1\n'
+    VCPKG_INSTALL = "vcpkg install --triplet={PLATFORM}-windows {PACKAGE}\n"
 
-VS = os.environ["APPVEYOR_BUILD_WORKER_IMAGE"]
-Config = os.environ["Configuration"]
-Platform = os.environ["Platform"]
+    s = ''.join([VCPKG_INSTALL.format(PLATFORM=Platform, PACKAGE=pkg) + ERROR_COMMAND for pkg in packages])
 
-VS_MAP = {
-    "Visual Studio 2017" : "Visual Studio 15 2017",
-    "Visual Studio 2015" : "Visual Studio 14 2015",
-    "Visual Studio 2013" : "Visual Studio 12 2013",
-    "Visual Studio 2012" : "Visual Studio 11 2012",
-    "Visual Studio 2010" : "Visual Studio 10 2010",
-    "Visual Studio 2008" : "Visual Studio 9 2008",
-}
+    with open("install.bat", "w") as f:
+        f.write(s)
 
-Generator = VS_MAP[VS]
-if Platform == "x64":
-    Generator += " Win64"
+def BuildScript():
+    print(os.environ["APPVEYOR_BUILD_WORKER_IMAGE"])
+    print(os.environ["Configuration"])
+    print(os.environ["Platform"])
 
-ERROR_COMMAND = 'IF %ERRORLEVEL% NEQ 0 EXIT /B 1'
-CMAKE_COMMAND1 = 'cmake -G"{GENERATOR}" -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake ..'.format(GENERATOR=Generator)
-CMAKE_COMMAND2 = 'cmake --build . --config ' + Config
+    VS = os.environ["APPVEYOR_BUILD_WORKER_IMAGE"]
+    Config = os.environ["Configuration"]
+    Platform = os.environ["Platform"]
 
-CMAKE_COMMANDS = ["mkdir build",
-    "cd build",
-    CMAKE_COMMAND1,
-    ERROR_COMMAND,
-    CMAKE_COMMAND2,
-    ERROR_COMMAND,
-    "cd ..",
-    "\n",
-]
+    VS_MAP = {
+        "Visual Studio 2017" : "Visual Studio 15 2017",
+        "Visual Studio 2015" : "Visual Studio 14 2015",
+        "Visual Studio 2013" : "Visual Studio 12 2013",
+        "Visual Studio 2012" : "Visual Studio 11 2012",
+        "Visual Studio 2010" : "Visual Studio 10 2010",
+        "Visual Studio 2008" : "Visual Studio 9 2008",
+    }
 
-with open("build.bat", "w") as f:
-    f.write("\n".join(CMAKE_COMMANDS))
+    Generator = VS_MAP[VS]
+    if Platform == "x64":
+        Generator += " Win64"
+
+    ERROR_COMMAND = 'IF %ERRORLEVEL% NEQ 0 EXIT /B 1\n'
+    CMAKE_COMMAND1 = 'cmake -G"{GENERATOR}" -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake ..\n'.format(GENERATOR=Generator)
+    CMAKE_COMMAND2 = 'cmake --build . --config ' + Config + '\n'
+
+    CMAKE_COMMANDS = ["mkdir build",
+        "cd build\n",
+        CMAKE_COMMAND1,
+        ERROR_COMMAND,
+        CMAKE_COMMAND2,
+        ERROR_COMMAND,
+        "cd ..\n",
+    ]
+
+    with open("build.bat", "w") as f:
+        f.write("".join(CMAKE_COMMANDS))
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2
+    mode = sys.argv[1]
+    assert mode in ("install", "build")
+    print(mode)
+
+    if mode == "install":
+        InstallScript()
+    elif mode == "build":
+        BuildScript()
